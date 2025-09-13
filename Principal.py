664b2f30346fb77cebf6fb32f0eaf6b9ea9f755a
Hugo <hugo@example.com>
2025-09-12 21:22:10 -0400

    Alteração programa principal

--- Principal.py
+++ Principal.py
@@ -1,3 +1,5 @@
+from programaSoma import programaSoma
+
 def imprimeMenu():
     print("Opções possíveis:")
     print("1 - Soma")
@@ -9,8 +11,12 @@ opcao = 99
 while opcao != 0:
     opcao = int(input("Digite a opção: "))
     
+    n1 = int(input("Digite o número 1: "))
+    n2 = int(input("Digite o número 2: "))
+
     if opcao == 1:
-        print("Somando ...")
+        pSoma = programaSoma()
+        print(f"Soma = {pSoma.adicao(n1,n2)}")
     elif opcao == 2:
         print("Subtraindo ...")
     elif opcao == 0:
